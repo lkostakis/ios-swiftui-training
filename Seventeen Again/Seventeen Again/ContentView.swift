70a0ab0664e8551a1efd4f8dcab57582f9d91f1d
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-07-12 16:16:53 +0300

    init

---
+++ Seventeen Again/Seventeen Again/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Seventeen Again
+//
+//  Created by Lefteris Kostakis on 12/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Seventeen Again/Seventeen Again/Seventeen_AgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seventeen_AgainApp.swift
+//  Seventeen Again
+//
+//  Created by Lefteris Kostakis on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Seventeen_AgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
