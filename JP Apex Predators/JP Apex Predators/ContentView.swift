9d7357d9b8bd2b1039b260447eefd93abe028679
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-07-13 17:45:40 +0300

    jurassic init

---
+++ JP Apex Predators/JP Apex Predators/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JP Apex Predators
+//
+//  Created by Lefteris Kostakis on 13/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
