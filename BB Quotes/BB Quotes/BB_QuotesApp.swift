269e8136118e48cea73f71bc04fd44f2dad517d6
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-07-14 10:17:39 +0300

    BB Quotes init

---
+++ BB Quotes/BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by Lefteris Kostakis on 14/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
