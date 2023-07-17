5af0a0ec4270b67d9b38c2945df49e2579dd2458
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-07-17 14:59:00 +0300

    pokemon detail & SamplePokemon

---
+++ Dex 3/Dex 3/PokemonDetail.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonDetail.swift
+//  Dex 3
+//
+//  Created by Lefteris Kostakis on 17/7/23.
+//
+
+import SwiftUI
+
+struct PokemonDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetail()
+    }
+}
---
+++ Dex 3/Dex 3/SamplePokemon.swift
@@ -0,0 +1,8 @@
+//
+//  SamplePokemon.swift
+//  Dex 3
+//
+//  Created by Lefteris Kostakis on 17/7/23.
+//
+
+import Foundation
