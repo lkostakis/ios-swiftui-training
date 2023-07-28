e5faf83ee3efe4cef98e8b46df4e872f3caecee1
Lefteris Kostakis <lp.kostakis@yahoo.gr>
2023-07-28 17:52:37 +0300

    HP Trivia initial commit

---
+++ HP Trivia/HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Lefteris Kostakis on 28/7/23.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
